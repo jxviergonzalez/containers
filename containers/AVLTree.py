f65a73ac667c031626e932e71afb8fb82e061043
Javier Gonzalez <Oscar.Gonzalez.24@cmc.edu>
2023-03-28 11:22:53 -0700

    pls work

--- containers/AVLTree.py
+++ containers/AVLTree.py
@@ -1,6 +1,7 @@
 '''
 This file implements the AVL Tree data structure.
-The functions in this file are considerably harder than the functions in the BinaryTree and BST files,
+The functions in this file are considerably harder than the functions in the
+BinaryTree and BST files,
 but there are fewer of them.
 '''
 
@@ -8,7 +9,7 @@ from containers.BinaryTree import BinaryTree, Node
 from containers.BST import BST
 
 
-class AVLTree():
+class AVLTree(BST):
     '''
     FIXME:
     AVLTree is currently not a subclass of BST.
@@ -21,6 +22,7 @@ class AVLTree():
         FIXME:
         Implement this function.
         '''
+        super().__init__()
 
     def balance_factor(self):
         '''
@@ -39,7 +41,8 @@ class AVLTree():
 
     def is_avl_satisfied(self):
         '''
-        Returns True if the avl tree satisfies that all nodes have a balance factor in [-1,0,1].
+        Returns True if the avl tree satisfies that all nodes have a balance
+        factor in [-1,0,1].
         '''
         return AVLTree._is_avl_satisfied(self.root)
 
@@ -49,6 +52,22 @@ class AVLTree():
         FIXME:
         Implement this function.
         '''
+        if node is None:
+            return True
+        ret = True
+        if node.left:
+            if AVLTree._balance_factor(node) >= -1 \
+                    and AVLTree._balance_factor(node) <= 1:
+                ret &= AVLTree._is_avl_satisfied(node.left)
+            else:
+                ret = False
+        if node.right:
+            if AVLTree._balance_factor(node) >= -1 \
+                    and AVLTree._balance_factor(node) <= 1:
+                ret &= AVLTree._is_avl_satisfied(node.right)
+            else:
+                ret = False
+        return ret
 
     @staticmethod
     def _left_rotate(node):
@@ -58,9 +77,21 @@ class AVLTree():
 
         The lecture videos provide a high-level overview of tree rotations,
         and the textbook provides full python code.
-        The textbook's class hierarchy for their AVL tree code is fairly different from our class hierarchy,
+        The textbook's class hierarchy for their AVL tree code is fairly
+        different from our class hierarchy,
         however, so you will have to adapt their code.
         '''
+        if node is None or node.right is None:
+            return node
+        newroot = Node(node.right.value)
+        newroot.right = node.right.right
+
+        newleft = Node(node.value)
+        newleft.left = node.left
+        newleft.right = node.right.left
+
+        newroot.left = newleft
+        return newroot
 
     @staticmethod
     def _right_rotate(node):
@@ -70,25 +101,58 @@ class AVLTree():
 
         The lecture videos provide a high-level overview of tree rotations,
         and the textbook provides full python code.
-        The textbook's class hierarchy for their AVL tree code is fairly different from our class hierarchy,
+        The textbook's class hierarchy for their AVL tree code is fairly
+        different from our class hierarchy,
         however, so you will have to adapt their code.
         '''
+        if node is None or node.left is None:
+            return node
+        newroot = Node(node.left.value)
+        newroot.left = node.left.left
+
+        newright = Node(node.value)
+        newright.right = node.right
+        newright.left = node.left.right
+
+        newroot.right = newright
+        return newroot
 
     def insert(self, value):
         '''
         FIXME:
         Implement this function.
 
-        The lecture videos provide a high-level overview of how to insert into an AVL tree,
+        The lecture videos provide a high-level overview of how to
+        insert into an AVL tree,
         and the textbook provides full python code.
-        The textbook's class hierarchy for their AVL tree code is fairly different from our class hierarchy,
+        The textbook's class hierarchy for their AVL tree code is
+        fairly different from our class hierarchy,
         however, so you will have to adapt their code.
 
         HINT:
         It is okay to add @staticmethod helper functions for this code.
-        The code should look very similar to the code for your insert function for the BST,
+        The code should look very similar to the code for your insert
+        function for the BST,
         but it will also call the left and right rebalancing functions.
         '''
+        super().insert(value)
+        self.root = AVLTree._insertv(self.root)
+        print("self.root=", self.root)
+        return self.root
+
+    @staticmethod
+    def _insertv(node):
+        if node is None:
+            return node
+        if AVLTree._balance_factor(node) < -1 \
+                or AVLTree._balance_factor(node) > 1:
+            node = AVLTree._rebalance(node)
+        if node.left:
+            node.left = AVLTree._insertv(node.left)
+        if node.right:
+            node.right = AVLTree._insertv(node.right)
+        print("node=", node)
+        return node
 
     @staticmethod
     def _rebalance(node):
@@ -98,3 +162,20 @@ class AVLTree():
         But both the insert function needs the rebalancing code,
         so I recommend including that code here.
         '''
+        if AVLTree._balance_factor(node) < 0:
+            if AVLTree._balance_factor(node.right) > 0:
+                node.right = AVLTree._right_rotate(node.right)
+                node = AVLTree._left_rotate(node)
+            else:
+                node = AVLTree._left_rotate(node)
+        elif AVLTree._balance_factor(node) > 0:
+            if AVLTree._balance_factor(node.left) < 0:
+                node.left = AVLTree._left_rotate(node.left)
+                node = AVLTree._right_rotate(node)
+            else:
+                node = AVLTree._right_rotate(node)
+        return node
+
+    def insert_list(self, xs):
+        for x in xs:
+            AVLTree.insert(self, value=x)
--- containers/BST.py
+++ containers/BST.py
@@ -1,6 +1,7 @@
 '''
 This file implements the Binary Search Tree data structure.
-The functions in this file are considerably harder than the functions in the BinaryTree file.
+The functions in this file are considerably harder than the
+functions in the BinaryTree file.
 '''
 
 from containers.BinaryTree import BinaryTree, Node
@@ -9,7 +10,8 @@ from containers.BinaryTree import BinaryTree, Node
 class BST(BinaryTree):
     '''
     The BST is a superclass of BinaryTree.
-    That means that the BST class "inherits" all of the methods from BinaryTree,
+    That means that the BST class "inherits" all of the
+    methods from BinaryTree,
     and we don't have to reimplement them.
     '''
 
@@ -19,20 +21,24 @@ class BST(BinaryTree):
         If xs is a list (i.e. xs is not None),
         then each element of xs needs to be inserted into the BST.
         '''
-        super().__init__()
+        self.root = None
+        if xs:
+            self.insert_list(xs)
 
     def __repr__(self):
         '''
         Notice that in the BinaryTree class,
         we defined a __str__ function,
         but not a __repr__ function.
-        Recall that the __repr__ function should return a string that can be used to recreate a valid instance of the class.
+        Recall that the __repr__ function should return
+        a string that can be used to recreate a valid instance of the class.
         Thus, if you create a variable using the command BST([1,2,3])
         it's __repr__ will return "BST([1,2,3])"
 
         For the BST, type(self).__name__ will be the string "BST",
         but for the AVLTree, this expression will be "AVLTree".
-        Using this expression ensures that all subclasses of BST will have a correct implementation of __repr__,
+        Using this expression ensures that all subclasses of BST
+        will have a correct implementation of __repr__,
         and that they won't have to reimplement it.
         '''
         return type(self).__name__ + '(' + str(self.to_list('inorder')) + ')'
@@ -52,13 +58,22 @@ class BST(BinaryTree):
         Convert the contents of both trees into a sorted list,
         then compare those sorted lists for equality.
         '''
+        if self is None or t2 is None:
+            return False
+        list_1 = self.to_list('inorder')
+        list_2 = t2.to_list('inorder')
+        if list_1 == list_2:
+            return True
+        else:
+            return False
 
     def is_bst_satisfied(self):
         '''
         Whenever you implement a data structure,
         the first thing to do is to implement a function that checks whether
         the structure obeys all of its laws.
-        This makes it possible to automatically test whether insert/delete functions
+        This makes it possible to automatically test whether
+        insert/delete functions
         are actually working.
         '''
         if self.root:
@@ -70,21 +85,28 @@ class BST(BinaryTree):
         '''
         FIXME:
         The current implementation has a bug:
-        it only checks if the children of the current node are less than/greater than,
-        rather than ensuring that all nodes to the left/right are less than/greater than.
+        it only checks if the children of the current node are
+        less than/greater than,
+        rather than ensuring that all nodes to the left/right
+        are less than/greater than.
 
         HINT:
         Use the _find_smallest and _find_largest functions to fix the bug.
-        You should use the _ prefixed methods because those are static methods just like this one.
+        You should use the _ prefixed methods because those
+        are static methods just like this one.
         '''
+        if node is None:
+            return True
         ret = True
         if node.left:
-            if node.value >= node.left.value:
+            if node.left.value < node.value and \
+               node.value > BST._find_largest(node.left):
                 ret &= BST._is_bst_satisfied(node.left)
             else:
                 ret = False
         if node.right:
-            if node.value <= node.right.value:
+            if node.right.value > node.value and \
+               node.value < BST._find_smallest(node.right):
                 ret &= BST._is_bst_satisfied(node.right)
             else:
                 ret = False
@@ -98,8 +120,26 @@ class BST(BinaryTree):
         Implement this function.
 
         HINT:
-        Create a staticmethod helper function following the pattern of _is_bst_satisfied.
+        Create a staticmethod helper function following the
+        pattern of _is_bst_satisfied.
         '''
+        if self.root:
+            BST._insert(self.root, value)
+        else:
+            self.root = Node(value)
+
+    @staticmethod
+    def _insert(node, value):
+        if value < node.value:
+            if node.left:
+                BST._insert(node.left, value)
+            else:
+                node.left = Node(value)
+        elif value > node.value:
+            if node.right:
+                BST._insert(node.right, value)
+            else:
+                node.right = Node(value)
 
     def insert_list(self, xs):
         '''
@@ -110,8 +150,11 @@ class BST(BinaryTree):
 
         HINT:
         Repeatedly call the insert method.
-        You cannot get this method to work correctly until you have gotten insert to work correctly.
+        You cannot get this method to work correctly until
+        you have gotten insert to work correctly.
         '''
+        for x in xs:
+            BST.insert(self, value=x)
 
     def __contains__(self, value):
         '''
@@ -126,6 +169,14 @@ class BST(BinaryTree):
         FIXME:
         Implement this function.
         '''
+        if self.root:
+            ret = BST._find(value, self.root)
+            if ret:
+                return ret
+            else:
+                return False
+        else:
+            return False
 
     @staticmethod
     def _find(value, node):
@@ -133,6 +184,14 @@ class BST(BinaryTree):
         FIXME:
         Implement this function.
         '''
+        if node is None:
+            return False
+        elif node.value == value:
+            return True
+        elif value < node.value:
+            return BST._find(value, node.left)
+        else:
+            return BST._find(value, node.right)
 
     def find_smallest(self):
         '''
@@ -146,7 +205,8 @@ class BST(BinaryTree):
     @staticmethod
     def _find_smallest(node):
         '''
-        This is a helper function for find_smallest and not intended to be called directly by the user.
+        This is a helper function for find_smallest and not
+        intended to be called directly by the user.
         '''
         assert node is not None
         if node.left is None:
@@ -164,6 +224,22 @@ class BST(BinaryTree):
         HINT:
         Follow the pattern of the _find_smallest function.
         '''
+        if self.root is None:
+            raise ValueError('Nothing in tree')
+        else:
+            return BST._find_largest(self.root)
+
+    @staticmethod
+    def _find_largest(node):
+        '''
+        This is a helper function for find_smallest and not
+        intended to be called directly by the user.
+        '''
+        assert node is not None
+        if node.right is None:
+            return node.value
+        else:
+            return BST._find_largest(node.right)
 
     def remove(self, value):
         '''
@@ -174,11 +250,51 @@ class BST(BinaryTree):
         Implement this function.
 
         HINT:
-        You should have everything else working before you implement this function.
+        You should have everything else working before you
+        implement this function.
 
         HINT:
         Use a recursive helper function.
         '''
+        if self.root is None:
+            return
+        else:
+            self.root = BST._remove(self.root, value)
+
+    @staticmethod
+    def _remove(node, value):
+        if node is None:
+            return node
+        if value < node.value:
+            node.left = BST._remove(node.left, value)
+            return node
+        elif value > node.value:
+            node.right = BST._remove(node.right, value)
+            return node
+        if node.value == value:
+            if node.left is None and node.right is None:
+                node = None
+                return node
+            if node.left is None:
+                temp = node.right
+                node = None
+                return temp
+            elif node.right is None:
+                temp = node.left
+                node = None
+                return temp
+            else:
+                succparent = node
+                succ = node.right
+                while succ.left is not None:
+                    succparent = succ
+                    succ = succ.left
+                if succparent != node:
+                    succparent.left = succ.right
+                else:
+                    succparent.right = succ.right
+                node.value = succ.value
+                return node
 
     def remove_list(self, xs):
         '''
@@ -190,3 +306,12 @@ class BST(BinaryTree):
         HINT:
         See the insert_list function.
         '''
+        for x in xs:
+            BST.remove(self, value=x)
+
+    def __iter__(self):
+        if self:
+            return self.root.__iter__()
+        else:
+            my_list = []
+            return my_list.__iter__()
