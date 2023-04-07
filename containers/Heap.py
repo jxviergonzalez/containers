387017b1a261f69477eabe1ae215a98b3de63558
Javier Gonzalez <Oscar.Gonzalez.24@cmc.edu>
2023-04-06 21:39:38 -0700

    final?

--- containers/Heap.py
+++ containers/Heap.py
@@ -1,4 +1,15 @@
+'''
+This file implements the Heap data structure as a subclass of the BinaryTree.
+The book implements Heaps using an *implicit* tree with an *explicit* vector
+implementation, so the code in the book is likely to be less helpful than the
+code for the other data structures. The book's implementation is the
+traditional implementation because it has a faster constant factor (but the
+same asymptotics). This  homework is using an explicit tree implementation to
+help you get more practice with OOP-style programming and classes.
+'''
+
 from containers.BinaryTree import BinaryTree, Node
+
 
 class Heap(BinaryTree):
     '''
